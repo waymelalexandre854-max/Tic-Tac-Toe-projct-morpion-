3ad34167fdcdea7d6f1a1fd99cc78ef77d32f5a4
mathisrenaudin14-ops <mathisrenaudin14@gmail.com>
2026-03-04 15:02:11 +0100

    Refactor easy AI logic in Tic Tac Toe
    Refactor easy AI implementation for Tic Tac Toe game. Improved readability and structure of the AI logic.

--- Easy_AI_tictactoe.py
+++ Easy_AI_tictactoe.py
@@ -1,40 +1,41 @@
 """AI easy tictactoe"""
-"""AI show nothing, it just take a choice depending on it's difficulty. Easy is mostly random"""
+"""This programm is the easy form of the AI for the tic tac toe game. It mostly play hasardly, only looking at all the possibility of his move."""
+"""it don't use minmax because we were trying to see other way to do programm other than minmax"""
 
 def easy_AI(Grid):
-    from random import choice
+    from random import choice        #choice from random give us a possibility from all possible one, resulting to a hazardous play
     from Recurring_function import verifgame, create_tree
-    play_list = []
-    best_value = 0
-    result_dico = {}
-    Tree = []
-    win = 0
-    x = 1
-    Tree = create_tree(Grid,2)
+    play_list = []        #This is the list of possibilities that the AI have at the end, one is selected at the end
+    best_value = 0        #The way to know what is the best way to win, put at 0 because the AI can't know player possibilities
+    result_dico = {}        #Where we input all the result to know what is the move that have the best value
+    Tree = []        #The tree of possibilities
+    win = 0        #a variable to say if the move is a win or a lose
+    x = 1        #a variable to change from one node to another
+    Tree = create_tree(Grid,2)        #create the tree of possibilities that the programm will study
     for i in Tree[1]:
-        win = verifgame(i)
+        win = verifgame(i)        #the value of win give point to the resepctive play
         if win == 0:
             result_dico[x] = 0
         elif win == 1:
             result_dico[x] = -1000
         else:
             result_dico[x] = 1000
-        if i == Grid:
+        if i == Grid:        #if the play look like the previouse one, it's removed from the dico
             del result_dico[x]
         x += 1
-    for key,value in result_dico.items():
+    for key,value in result_dico.items():        #look for the best value in the dico
         if best_value < value:
             best_value = value
-    for key,value in result_dico.items():
+    for key,value in result_dico.items():        #create the list of possibilitties
         if value == best_value:
             play_list.append(key)
 
-    return choice(play_list)
+    return choice(play_list)        #send back a number which is where the AI is playing
 
 
 """Python form of the game with Easy AI implemented"""
 
-def tictactoe():
+def tictactoe():        #the start is mostly like tictactoe_pvp_game_programm_v1, only little modification to make using AI possible
     from random import randint
     from Easy_AI_tictactoe import easy_AI
     from Recurring_function import verifgame
@@ -78,7 +79,7 @@ def tictactoe():
                 return "player 1 win", Grid
             if y == 2:
                 return "player 2 win", Grid
-    elif PV == 2:
+    elif PV == 2:        #this is where the player versus AI programm start, the player move is like the on in PVP, but AI use the easy_AI function
         for i in range(9):
             if player == 1:
                 while good_play == False:
@@ -97,7 +98,7 @@ def tictactoe():
                 good_play = False
                 player = 2
             else:
-                if Type_AI == 1:
+                if Type_AI == 1:        #with more AI, the Type_AI can be the way to know the difference between each AI
                     Ai = easy_AI(Grid)
                     if Ai == 1 or Ai == 2 or Ai == 3:
                         Grid[0][Ai-1] = player
@@ -117,3 +118,4 @@ def tictactoe():
                 return "player 2 win", Grid
     return "no player have win this match"
 print(tictactoe())
+
